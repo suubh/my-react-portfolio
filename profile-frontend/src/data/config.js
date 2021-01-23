fc802d9ea0e0264181d50e080a8613aca46dc4b5
suubh <subhdec99@gmail.com>
2021-01-23 21:19:01 +0530

    Add files via upload

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Shubham Singh",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Shubham Singh and I'm a Software Engineer!",
+  githubUrl: "https://github.com/suubh",
+  linkedinUrl: "https://www.linkedin.com/in/shubham-singh-356ba5168",
+  cvLink: "https://drive.google.com/file/d/1OwK59LcP2sNYMwCdesYocjN0vQujWYb0/view",
+};
